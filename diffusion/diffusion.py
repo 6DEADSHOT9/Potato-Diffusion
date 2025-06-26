4973b5b5ab9a13a17ff46d62384e95ea73e36f8d
6DEADSHOT9 <arora.dhruv26@gmail.com>
2025-06-26 14:22:59 +0530

    model converter added to enable inference

--- diffusion/diffusion.py
+++ diffusion/diffusion.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
-from diffblocks import SwitchSequential, UNET_ResidualBlock, UNET_AttentionBlock, Upsample
+from diffblocks import SwitchSequential, UNET_ResidualBlock, UNET_AttentionBlock, Upsample, TimeEmbedding
 
 
 class UNET(nn.Module):
@@ -64,6 +64,26 @@ class UNET(nn.Module):
             SwitchSequential(UNET_ResidualBlock(640, 320), UNET_AttentionBlock(8,40)),
         
         ])
+    
+    def forward(self, x, context, time):
+        # x: (Batch_Size, 4, Height / 8, Width / 8)
+        # context: (Batch_Size, Seq_Len, Dim) 
+        # time: (1, 1280)
+
+        skip_connections = []
+        for layers in self.encoders:
+            x = layers(x, context, time)
+            skip_connections.append(x)
+
+        x = self.bottleneck(x, context, time)
+
+        for layers in self.decoders:
+            # Since we always concat with the skip connection of the encoder, the number of features increases before being sent to the decoder's layer
+            x = torch.cat((x, skip_connections.pop()), dim=1) 
+            x = layers(x, context, time)
+        
+        return x
+
 
 class UNET_OutputLayer(nn.Module):
 
@@ -80,3 +100,28 @@ class UNET_OutputLayer(nn.Module):
             x = self.conv(x)
     
             return x
+
+
+class Diffusion(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.time_embedding = TimeEmbedding(320)
+        self.unet = UNET()
+        self.final = UNET_OutputLayer(320, 4)
+    
+    def forward(self, latent, context, time):
+        # latent: (Batch_Size, 4, Height / 8, Width / 8)
+        # context: (Batch_Size, Seq_Len, Dim)
+        # time: (1, 320)
+
+        # (1, 320) -> (1, 1280)
+        time = self.time_embedding(time)
+        
+        # (Batch, 4, Height / 8, Width / 8) -> (Batch, 320, Height / 8, Width / 8)
+        output = self.unet(latent, context, time)
+        
+        # (Batch, 320, Height / 8, Width / 8) -> (Batch, 4, Height / 8, Width / 8)
+        output = self.final(output)
+        
+        # (Batch, 4, Height / 8, Width / 8)
+        return output
